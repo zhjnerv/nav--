9cf18706475f32eac1450fd188398ecad0c97260
BAYUEQI <zouqi050813@gmail.com>
2025-06-04 21:41:07 +0800

    添加了随机一言接口

--- worker.js
+++ worker.js
@@ -1645,7 +1645,17 @@ function renderSiteCard(site) {
               ${catalog ? `${currentCatalog} · ${currentSites.length} 个网站` : `全部收藏 · ${sites.length} 个网站`}
             </h2>
             <div class="text-sm text-gray-500 hidden md:block">
-              点击卡片访问网站，鼠标悬停可复制链接
+              <script>
+                 fetch('https://v1.hitokoto.cn')
+                      .then(response => response.json())
+                      .then(data => {
+                       const hitokoto = document.getElementById('hitokoto_text')
+                      hitokoto.href = 'https://hitokoto.cn/?uuid=' + data.uuid
+                      hitokoto.innerText = data.hitokoto
+                      })
+                      .catch(console.error)
+              </script>
+              <div id="hitokoto"><a href="#" target="_blank" id="hitokoto_text">疏影横斜水清浅，暗香浮动月黄昏。</a></div>
             </div>
           </div>
           
